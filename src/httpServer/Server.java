eda0e963a9a8130b82f90e744c051ff9f2605f29
Pranav <pranav.dilip.patel@gmail.com>
2018-02-23 01:51:24 +0100

    some changes to server thread in hopes of making it work

--- src/httpServer/Server.java
+++ src/httpServer/Server.java
@@ -6,19 +6,20 @@ import java.net.Socket;
 
 public class Server {
     
-    private static final int MYPORT = 8888;
+    private static final int MYPORT = 420;
     
 	public static void main(String[] args) throws IOException {
 		
             ServerSocket sSocket = null;
             sSocket = new ServerSocket(MYPORT);
-          
+            System.out.println("Server has begun running!");
             
             int ID = 0;
             
             while (true) {
                 Socket soc = sSocket.accept();
                 ServerThread serverThread = new ServerThread(soc, ++ID);
+                serverThread.runServer();
             }
 	}
 
--- src/httpServer/ServerThread.java
+++ src/httpServer/ServerThread.java
@@ -5,19 +5,62 @@
  */
 package httpServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-
+import java.util.stream.Collectors;
 
 /**
  *
  * @author Pranav
  */
-public class ServerThread {
+public class ServerThread extends Thread {
+
     private static int ID;
     private Socket soc;
-    
+    private byte[] buf;
+    private int BUFSIZE = 1024;
+    private InputStreamReader streamIn;
+    private PrintWriter write;
+    private BufferedReader read;
+
     public ServerThread(Socket socket, int identity) {
         soc = socket;
         ID = identity;
+    }
+
+    public void runServer() {
+        String message = "";
+
+        try {
+            streamIn = new InputStreamReader(soc.getInputStream());
+            read = new BufferedReader(streamIn);
+            String line = read.readLine();
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = read.readLine();
+            }
+            PrintWriter out = new PrintWriter(soc.getOutputStream());
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-type: text/html");
+            out.println("\r\n");
+            InputStream in = this.getClass().getClassLoader()
+            .getResourceAsStream("test.html");
+            String s = new BufferedReader(new InputStreamReader(in))
+            .lines().collect(Collectors.joining("\n"));
+            out.println(s);
+            /*In this line I used out.println("full html code"); but I'd like a 
+            simpler way where it can search for the html file in the directory and send it.*/
+            out.flush();
+            out.close();
+            soc.close();
+        } catch (IOException e) {
+            System.out.println("Esteban wtf are you doing?!");
+            e.printStackTrace();
+        }
+
     }
 }
